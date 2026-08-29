4db099de54e0092b5eb5c8c46ed727b697bdbe09
Alif Putra Winata <98870352+arfshl@users.noreply.github.com>
2026-08-29 22:34:33 +0700

    Update generate-icons.sh

--- generate-icons.sh
+++ generate-icons.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for size in 16 32 48 96 128 256 512; do
+for size in 16 32 48 64 96 128 256 512; do
     inkscape icon.svg \
         --export-filename="${size}.png" \
         --export-width="$size" \
